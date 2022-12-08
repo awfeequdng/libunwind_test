236db04f4cb52d9365f474adff8448202ecb3257
pengxiangcai <pxcai@abeliancap.com>
2022-12-09 01:04:03 +0800

    use libunwind to skip function

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ CXX=clang++
 all:
 	${CXX} -g test.cc -o test
 	${CXX} -g unwind_test.cc -o unwind_test -lunwind -lunwind-generic -Wl,-rpath=./libunwind/lib -I./libunwind/include
+	${CXX} -g unwind_test1.cc -o unwind_test1 -lunwind -lunwind-generic -Wl,-rpath=./libunwind/lib -I./libunwind/include
 
 clean:
-	rm -rf test unwind_test
+	rm -rf test unwind_test unwind_test1
---
+++ unwind_test1.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+// libunwind的编译过程：
+// autoreconf -i
+// ./configure
+// make
+// make install
+//
+#include "libunwind.h"
+
+void skip_func1() {
+    unw_cursor_t cursor;
+    unw_context_t uc;
+
+    unw_getcontext(&uc);
+    unw_init_local(&cursor, &uc);
+    unw_step(&cursor);
+    unw_resume(&cursor);
+    std::cout << "skip_func1 will be skipped" << std::endl;
+}
+
+void skip_func2() {
+    unw_cursor_t cursor;
+    unw_context_t uc;
+
+    unw_getcontext(&uc);
+    unw_init_local(&cursor, &uc);
+    unw_step(&cursor);
+    unw_step(&cursor);
+    unw_resume(&cursor);
+    std::cout << "skip_func2 will be skipped" << std::endl;
+}
+
+void func1() {
+    skip_func1();
+    std::cout << "func1 will not be skipped" << std::endl;
+}
+
+void func2() {
+    skip_func2();
+    std::cout << "func2 will be skipped" << std::endl;
+}
+
+int main() {
+    std::cout << "start" << std::endl;
+    func1();
+    std::cout << "func1 finished" << std::endl;
+    func2();
+    std::cout << "end" << std::endl;
+    return 0;
+}
